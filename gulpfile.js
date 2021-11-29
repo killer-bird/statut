aeab41590df8e1457f0b3f228f4d6d8a024e2386
Rommel <rommel056@gmail.com>
2021-11-29 09:59:25 +0300

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass =  require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso')
 const include = require('gulp-file-include')
 const htmlmin =require('gulp-htmlmin')
 const del = require('del')
-const sync = require('browser-sync')
+const autroprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+const sync = require('browser-sync');
+const { series } = require('gulp');
+
 
 function html(){
   return gulp.src('index.html')
@@ -13,5 +17,24 @@ function html(){
   }))
   .pipe(gulp.dest('dist'))
 }
+function scss(){
+  return gulp.src('src/scss/**.scss')
+  .pipe(sass())
+  .pipe(csso())
+  .pipe(concat('index.css'))
+  .pipe(gulp.dest('dist'))
+}
+function clear(){
+  return del('dist')
+}
+function serve(){
+  sync.init({
+    server:'./dist'
+  })
+  gulp.watch('index.html', series(clear,html)).on('change', sync.reload)
+  gulp.watch('src/scss/**.scss', series(clear,scss)).on('change', sync.reload)
   
-exports.html = html
+}
+exports.build = gulp.series(clear, scss, html)
+exports.serve = series(clear, scss, html, serve)
+exports.clear = clear
