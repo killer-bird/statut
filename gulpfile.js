25d4ba2061be0400f48b375137a6e260c151d8fd
SoslanZS <soslan_zs@mail.ru>
2021-12-03 17:50:54 +0300

    add slider add book card

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
 const gulp = require('gulp')
 const sass =  require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso')
+const twig = require('gulp-twig')
 const include = require('gulp-file-include')
 const htmlmin =require('gulp-htmlmin')
 const del = require('del')
 const autroprefixer = require('gulp-autoprefixer')
 const concat = require('gulp-concat')
 const sync = require('browser-sync');
-const { series } = require('gulp');
 
 
-function html(){
-  return gulp.src('index.html')
-  .pipe(include({
-    prefix:'@@'
-  }))
+function twigCompale(){
+  return gulp.src('src/index.twig')
+  .pipe(twig())
   .pipe(gulp.dest('dist'))
 }
 function scss(){
-  return gulp.src(['src/scss/**.scss', 'node_modules/'])
+  return gulp.src(['src/assets/scss/**.scss', 'node_modules/'])
   .pipe(sass({
     includePaths: ['node_modules'] 
   }))
   .pipe(csso())
   .pipe(concat('index.css'))
-  .pipe(gulp.dest('dist/css'))
+  .pipe(gulp.dest('dist/assets/css'))
 }
 function images(){
-  return gulp.src(['src/img/**'])
-  .pipe(gulp.dest('dist/img'))
+  return gulp.src(['src/assets/img/**'])
+  .pipe(gulp.dest('dist/assets/img'))
+}
+function fonts(){
+  return gulp.src('src/assets/fonts/**')
+  .pipe(gulp.dest('dist/assets/fonts'))
 }
 function clear(){
   return del('dist')
@@ -37,10 +39,10 @@ function serve(){
   sync.init({
     server:'./dist'
   })
-  gulp.watch(['index.html', 'src/includes/**.html'], series(clear,scss,html, images )).on('change', sync.reload)
-  gulp.watch('src/scss/**.scss', series(clear,scss,html, images)).on('change', sync.reload)
+  gulp.watch(['src/index.twig', 'src/blocks/**.twig'], gulp.series(clear,scss,twigCompale, fonts, images)).on('change', sync.reload)
+  gulp.watch('src/assets/scss/**.scss', gulp.series(clear,scss,twigCompale,fonts, images)).on('change', sync.reload)
 }
 
-exports.build = gulp.series(clear, scss, html, images)
-exports.serve = series(clear, scss, html,images, serve, )
+exports.build = gulp.series(clear, scss, twigCompale, images, fonts)
+exports.serve = gulp.series(clear, scss, twigCompale,images, fonts, serve  )
 
