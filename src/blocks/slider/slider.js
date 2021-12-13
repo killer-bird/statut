9b00262e41e76a19f9f1d10fc6f92957b325f896
SoslanZS <soslan_zs@mail.ru>
2021-12-13 12:49:38 +0300

    ended slider

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,32 +1,77 @@
-let pos = 0;
-const show = 1;
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const slider = document.querySelector('.slider');
 const items = document.querySelectorAll('.slider__item');
 const item = document.querySelector('.slider__item');
-const itemWidth = slider.offsetWidth / show;
-const sliderLen = itemWidth * items.length;
+const dots = document.querySelectorAll('.slider__dots__item')
 
+let pos = 0;
+let current = 0;
 
+const sliderLen = slider.offsetWidth * items.length - slider.offsetWidth;
+
+const rangeKeeper = (val)=>{
+    if(val > 0){
+        return -Math.abs(sliderLen)
+    }
+    if(val < sliderLen) {
+        return 0
+    }
+}
 items.forEach(item=>{
-    item.style.minWidth = itemWidth + 'px';
-    console.log('done')
+    item.style.minWidth = slider.offsetWidth + 'px';
+    console.log('done');
 })
 
 next.addEventListener('click', ()=>{
-
-    pos-=itemWidth
+    pos-=slider.offsetWidth;
+    dots[current].classList.remove("--active");
+    
+    if(pos < -sliderLen){
+        pos = 0;
+        dots[current].classList.remove("--active");
+        current = 0;
+        dots[current].classList.add("--active");
+    }else{
+        current++;
+        dots[current].classList.add("--active");
+    }
     items.forEach(item=>{
-        item.style.left = pos + 'px'
+        item.style.left = pos + 'px';
     })
-
+    console.log(current)
     
 })
 prev.addEventListener('click', ()=>{
-    pos+=itemWidth
+    pos+=slider.offsetWidth;
+    dots[current].classList.remove("--active");
+    
+    if( pos > 0 ){
+        pos = -sliderLen;
+        dots[current].classList.remove("--active");
+        current = 3;
+        dots[current].classList.add("--active");
+    }else{
+        current--;
+        dots[current].classList.add("--active");
+    }
     items.forEach(item=>{
-        item.style.left = pos + 'px'
+        item.style.left = pos + 'px';
     })
 
+})
+dots.forEach((dot, index)=>{
+    dot.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('--active')){
+            return
+        }
+        items.forEach(item=>{
+            pos = index * -slider.offsetWidth;
+            dots[current].classList.remove("--active");
+            current = index;
+            dots[current].classList.add("--active");
+            item.style.left = pos + 'px';
+        })
+        
+    })
 })
